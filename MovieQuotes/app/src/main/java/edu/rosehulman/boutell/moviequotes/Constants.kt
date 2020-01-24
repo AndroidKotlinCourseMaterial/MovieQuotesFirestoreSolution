e988045d92dc3096203ad083b2337c9bc4738bae
Matt Boutell <boutell@gmail.com>
2020-01-24 17:41:46 -0500

    First commit in several that are a migration to a new solution that is easier to follow. Most changes are trivial.

--- MovieQuotes/app/src/main/java/edu/rosehulman/boutell/moviequotes/Constants.kt
+++
@@ -1,7 +0,0 @@
-package edu.rosehulman.boutell.moviequotes
-
-// Helpful to use with debugging.
-object Constants {
-    const val TAG = "MQ"
-    const val QUOTES_COLLECTION = "quotes"
-}
---
+++ MovieQuotes/app/src/main/java/edu/rosehulman/moviequotes/Constants.kt
@@ -0,0 +1,8 @@
+package edu.rosehulman.moviequotes
+
+object Constants {
+    const val TAG = "MQ"
+    const val QUOTES_COLLECTION = "quotes"
+    const val SETTINGS_COLLECTION = "settings"
+    const val SETTINGS_DOCUMENT = "settings"
+}
